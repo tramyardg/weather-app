f525788aeb785ce3cc1371662cc3c503d2eba925
Raymart DG <tramyardg@gmail.com>
2021-12-21 07:58:37 -0500

    + added sass

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     const callBackendAPI = async () => {
-      const response = await fetch('/express_backend');
+      const response = await fetch('/api');
       const body = await response.json();
 
       if (response.status !== 200) {
@@ -19,21 +19,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-app">
+      asd
     </div>
   );
 }
