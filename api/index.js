796b9a546d15d804e641ea4c2ad6084cb2390c54
Raymart DG <tramyardg@gmail.com>
2021-12-21 19:34:27 -0500

    update express

--- api/index.js
+++ api/index.js
@@ -7,3 +7,11 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/api', (req, res) => {
     res.send({ message: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+// app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+module.exports = app;
