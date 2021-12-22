c065432a74d4dafba6be80b197c04dc63af37504
Raymart DG <tramyardg@gmail.com>
2021-12-22 18:25:56 -0500

    fix util

--- client/src/helpers/util.js
+++ client/src/helpers/util.js
@@ -1,5 +1,6 @@
 export const getDayOfWeek = (idx) => {
     // https://day.js.org/docs/en/get-set/day
     // 0 (Sunday) to 6 (Saturday)
-    return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].at(idx);
+    const _days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return _days[idx];
 }
